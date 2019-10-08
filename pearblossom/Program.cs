c9f789bd0cdbfe525e8808621a268a42855e0d05
Angela-1 <ruoshui_engr@163.com>
2019-10-08 16:41:10 +0800

    UPDATE: optmized form async/await.

--- pearblossom/Program.cs
+++ pearblossom/Program.cs
@@ -29,7 +29,7 @@ namespace pearblossom
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
